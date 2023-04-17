1f4231fb5d9bee2d9fe80e5d85f770cc47c3abdd
Eugen Sadovnikov <sadeugen@gmail.com>
2023-04-18 00:59:40 +0300

    feat: begin implemantation binary-search-tree.js

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -97,13 +97,25 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!this.r) {return;}
+    let node = this.r;
+    while (node.left) {
+      node=node.left;
+    }
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!this.r) {return;}
+    let node = this.r;
+    while (node.right) {
+      node=node.right;
+    }
+    return node.data;
   }
 }
 
